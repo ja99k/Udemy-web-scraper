021b21fd79a66d127d17dbf3e6e7cde9caece069
ja99k <jannikn112@gmail.com>
2022-12-21 15:01:59 +0100

    More scraping

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/')
 soup = BeautifulSoup(res.text, 'html.parser')
-links = (soup.select('.titleline'))
-score = (soup.select('.score'))
+links = (soup.select('.titleline > a'))
+votes = (soup.select('.score'))
 
-# print(links[0])
-print(score[0])
+def sort_stories_by_points(hnlist):
+    return sorted(hnlist, reverse=True, key=lambda k:k['points'])
+
+def create_custom_hn(links, votes):
+    hn = []
+    for inx, item in enumerate(links):
+        title = links[inx].getText()
+        href = links[inx].get('href')
+        points = int(votes[inx].getText().replace(' points', ''))
+        if points < 100:
+            continue
+        # print(points)
+        hn.append({'title': title, 'link': href, 'points':points})
+    return sort_stories_by_points(hn)
+
+
+pprint.pprint((create_custom_hn(links, votes)))
