1b4eb1ad39d5929b52bb6a918d7a405f77202ee7
ja99k <jannikn112@gmail.com>
2022-12-21 11:56:54 +0100

    this is more scraping

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/')
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup)
+links = (soup.select('.titleline'))
+score = (soup.select('.score'))
 
+# print(links[0])
+print(score[0])
