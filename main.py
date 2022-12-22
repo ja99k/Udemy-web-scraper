b3cf2a988817e356a274198538d8031efea0789c
ja99k <jannikn112@gmail.com>
2022-12-22 17:00:22 +0100

    Added comments section to the dictionary.

--- main.py
+++ main.py
@@ -9,24 +9,28 @@ soup2 = BeautifulSoup(res2.text, 'html.parser')
 
 links = (soup.select('.titleline > a'))
 links2 = (soup2.select('.titleline > a'))
-subtext = (soup.select('.subtext'))
+subline = (soup.select('.subline'))
+soup_age = (soup.select('.age'))
 
 def sort_stories_by_points(hnlist):
     return sorted(hnlist, reverse=True, key=lambda k:k['points'])
 
-def create_custom_hn(links, subtext):
+def create_custom_hn(links, subline):
     hn = []
     for inx, item in enumerate(links):
         title = links[inx].getText()
         href = links[inx].get('href')
-        comments = subtext[inx].getText().replace('&nbps;comments', '')
-        vote = subtext[inx].select('.score')
+        comments = subline[inx].getText().replace('.age', '')
+        vote = subline[inx].select('.score')
         if len(vote):
             points = int(vote[0].getText().replace(' points', ''))
             if points > 99:
+                parts = comments.split("|")
+                comments = parts[-1].strip()
+                comments = comments.replace('\xa0', ' ')
                 hn.append({'title': title, 'link': href, 'points':points, 'comments':comments})
             # the comments section is not working properly
     return sort_stories_by_points(hn)
 
 
-pprint.pprint((create_custom_hn(links, subtext)))
+pprint.pprint((create_custom_hn(links, subline)))
